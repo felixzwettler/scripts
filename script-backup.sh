540a148aa49b643f7697d4bb914c468e0f5eb3e5
felixzwettler <f.zwettler@posteo.de>
2021-03-14 20:50:00 +0100

    only keep newest backups

--- script-backup.sh
+++ script-backup.sh
@@ -1,16 +1,24 @@
-#!/usr/bin/env bash
+#!/bin/bash
 ## backup script
 
 BACKUPTIME=$(date +%Y-%m-%d_%H-%M-%S)
 
+# Keep number of newest backups
+KEEPNO=14
+
 # Directories to backup
 BACKUPSOURCES="/home /etc /opt/ /var/www"
 
-BACKUPDEST="/media/cloud/Systeme/fz-cloud/backups/backup-$BACKUPTIME.tar.gz"
+# Target directory (no trailing slash!)
+BACKUPDIR="/path/to/backups"
+
+BACKUPDEST="${BACKUPDIR}/backup-${BACKUPTIME}.tar.gz"
 
 # Backing up using gzipped tar
 tar -cpzf $BACKUPDEST $BACKUPSOURCES
-chmod pi:pi $BACKUPDEST
+chown pi:pi $BACKUPDEST
+cd $BACKUPDIR
+find . -maxdepth 1 -name 'backup-*.tar.gz' -type f -printf '%T@ %p\n' | sort -n | sed "s/^[^ ]* //" | head -n -$KEEPNO | xargs rm -vf
 
 # Restore with for example:
-# tar -xpzf "/media/cloud/Systeme/fz-cloud/backup-2020-01-01_03-00-00.tgz"
+# tar -xpzf "/path/to/backups/backup-2020-01-01_03-00-00.tgz"
